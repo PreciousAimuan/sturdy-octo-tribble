24eb15065f440d53930240149c4bfc3081077cc6
Precious Aimuan <preciousaimuan@decagon.dev>
2024-01-21 23:41:37 +0100

    First C# Algorithm

---
+++ Documents/FirstSession/Algo1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSession
+{
+    public class Algo1
+    {
+        /*public string JoinTwoString(string a, string b)*/ //First Method
+        public static string JoinTwoString(string a, string b) //Second Method
+        {
+            string result = a +" "+b;
+            return result;
+
+        }
+    }
+}
